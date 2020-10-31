227e6cde7f2d6556a13f8ad58e379f50dbb1a225
yente06 <yente.heus@gmail.com>
2020-10-31 16:43:58 +0100

    Added the generateRandomNumber function

--- main.asm
+++ main.asm
@@ -9,6 +9,40 @@ SCRHEIGHT EQU 200   ;Pixels scherm hoogte
 VMEMADR EQU 0A0000h
 
 CODESEG
+;##FOR DEBUGGING##
+PROC printUnsignedInteger
+	ARG @@printval:dword
+	USES eax, ebx, ecx, edx
+	sti  ; Set the interrupt flag => enable interrupts.
+    cld  ; Clear the direction flag => auto-increment source
+          ; and destination indices.
+
+	mov	eax, [@@printval]  ; eax holds input integer
+	mov	ebx, 10		; divider
+	xor ecx, ecx	; counter for digits to be printed
+
+	; Store digits on stack
+@@getNextDigit:
+	inc	ecx         ; increase digit counter
+	xor edx, edx
+	div	ebx   		; divide by 10
+	push dx			; store remainder on stack
+	test eax, eax	; {cmp eax, 0} check whether zero?
+	jnz	@@getNextDigit
+
+    ; Write all digits to the standard output
+	mov	ah, 2h 		; Function for printing single characters.
+@@printDigits:
+	pop dx
+	add	dl,'0'      	; Add 30h => code for a digit in the ASCII table, ...
+	int	21h            	; Print the digit to the screen, ...
+	loop @@printDigits	; Until digit counter = 0.
+
+	ret
+ENDP printUnsignedInteger
+;##FOR DEBUGGING##
+
+
 PROC setVideoMode
 ARG @@mode:word
 USES eax
@@ -53,20 +87,45 @@ loop @@govertical
 ret
 ENDP drawTrex
 
+PROC generateRandomNumber ; Het gegenereerde getal komt terecht in [RandomState], gebaseerd op https://en.wikipedia.org/wiki/Xorshift
+USES EAX, EDX
+mov EAX, [RandomState]
+mov EDX, [RandomState]
+SHL EDX, 13							; Left shift met 13
+XOR EAX, EDX						; Sla de xor van EAX en EDX op in EAX
+mov EDX, EAX
+SHR EDX, 17							; Right shift met 17
+XOR EAX, EDX						; Sla de xor van EAX en EDX op in EAX
+mov EDX, EAX
+SHL EDX, 5							; Left shift met 5
+XOR EAX, EDX						; Sla de xor van EAX en EDX op in EAX
+mov [RandomState], EAX
+;DEBUGGING
+;call printUnsignedInteger, [RandomState]
+;mov ah, 09h ;lets int 21h know it needs to print a string
+;mov edx, offset NewLine
+;int 21h ; interrupts to print string pointed to by edx
+;;
+ret
+ENDP generateRandomNumber
+
 
 PROC main
 	sti
 	cld
+	call generateRandomNumber
+	;call setVideoMode, 12h
+  ;  call drawTrex, offset Trex, offset Size
 
-	call setVideoMode, 12h
-    call drawTrex, offset Trex, offset Size
-
-    mov	ah,00h
-	int	16h
+	mov ah,0h		; wait for keystroke
+	int 16h
+	mov	ax,4C00h 	; terminate
+	int 21h
 
 ENDP main
 
 DATASEG
+; Drawing sprites
 Size DW  32, 4
 Trex DB 00H, 00H, 00H, 00H
 	 	 DB 00H, 00H, 03FH, 0FCH
@@ -100,6 +159,9 @@ Trex DB 00H, 00H, 00H, 00H
 		 DB 00H, 03CH, 0F0H, 00H
 		 DB 00H, 03CH, 0F0H, 00H
 		 DB 00H, 00H, 00H, 00H
+;Random Generation
+RandomState DD 1957386613 ; Binary: 111 0100 1010 1011 0101 1001 0111 0101
+NewLine db ' ', 13, 10, '$' ; 13, 10: newline, $: eindigd interrupt
 
 STACK 100h
 
