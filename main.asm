f3878d1868c843c1aada5f3c7619fc57a93af2c9
yente06 <yente.heus@gmail.com>
2020-11-05 14:28:04 +0100

    Some more comments

--- main.asm
+++ main.asm
@@ -64,11 +64,11 @@ ARG @@symb:dword, @@width:dword
 USES EDI, EBX, EAX
 mov EDI, VMEMADR	;Geheugen adres
 mov EBX, [@@symb]	;adres Hex waarde
-mov AX, [EBX]			; Hex waarde
-xor ebx, ebx
+mov AL, [EBX]			; Hex waarde
 
 mov EBX, [@@width]
 mov CX, [EBX]				;grote
+; Nu zit in AL: waarde en BX: Grootte
 REP STOSB
 ret
 ENDP drawFloor
@@ -137,7 +137,7 @@ PROC main
 	cld
 	;call generateRandomNumber
 	call setVideoMode, 12h
-  call drawTrex, offset Trex, offset Size
+  ;call drawTrex, offset Trex, offset Size
 	call drawFloor, offset Floor, offset SizeFloor
 
 	mov ah,0h		; wait for keystroke
@@ -183,7 +183,7 @@ Trex DB 00H, 00H, 00H, 00H
 		 DB 00H, 03CH, 0F0H, 00H
 		 DB 00H, 00H, 00H, 00H
 
-SizeFloor DW 2
+SizeFloor DW 800
 Floor DB 0ffH
 ;Random Generation
 RandomState DD 0; 1957386613 Binary: 111 0100 1010 1011 0101 1001 0111 0101
